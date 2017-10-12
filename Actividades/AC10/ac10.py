9cf7c210911fd089143a123a2d728053c08b741f
Florencia Barrios <fbarrios1@uc.cl>
2017-10-12 14:23:22 -0300

    Enunciado AC10

---
+++ Actividades/AC10/ac10.py
@@ -0,0 +1,54 @@
+
+class Bicicleta:
+
+    def __init__(self):
+        pass
+
+    def prox_averia(self):
+        pass
+
+    def run(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class Tecnico:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+class Rescate:
+    def __init__(self):
+        pass
+
+    def agregar_participante(self):
+        pass
+
+    def run(self):
+        pass
+
+
+class Genio:
+    lock = "esto podria ser un lock..."
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    @staticmethod
+    def murcielago(text):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    pass
+    #gl & hf
